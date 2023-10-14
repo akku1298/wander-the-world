173c412f0db9f0034f247336453605760aa79dd0
Akansha Srivastav <67710450+akku1298@users.noreply.github.com>
2023-10-14 12:29:27 +0530

    first commit

---
+++ src/components/Tours.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Card from './Card'
+
+const Tours = ({tours, removeTour}) => {
+  return (
+    <div className="container">
+        <div>
+            <h2 className="title"> Wander the World</h2>
+        </div>
+        <div className="cards">
+            {
+                tours.map((tour) => {
+                    return <Card key={tour.id} {...tour} removeTour={removeTour}></Card>
+                })
+            }
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Tours
---
+++ src/data.js
@@ -0,0 +1,76 @@
+const data = [
+	{
+		id: 1,
+		name: "Agra",
+		info: "Agra is a city in northern India, known for the iconic Taj Mahal, a stunning white marble mausoleum built by Mughal emperor Shah Jahan in memory of his beloved wife. Agra is also home to several other historic and architectural wonders, including Agra Fort and Fatehpur Sikri.",
+		image: "https://cdn.thecodehelp.in/Agra.jpeg",
+		price: "15,758",
+	},
+	{
+		id: 2,
+		name: "Jaipur",
+		info: `Jaipur is the capital city of the Indian state of Rajasthan, known for its stunning pink-hued buildings and palaces. The city is also known as the "Pink City" due to the color of many of its buildings. Jaipur is famous for its intricate textiles, colorful handicrafts, and delicious cuisine.`,
+		image: "https://cdn.thecodehelp.in/Jaipur.jpeg",
+		price: "22,560",
+	},
+	{
+		id: 3,
+		name: "Goa",
+		info: " Goa is a small state on India's western coast, known for its stunning beaches, vibrant nightlife, and Portuguese-influenced architecture. The state is also known for its spicy seafood dishes and unique cultural traditions.",
+		image: "https://cdn.thecodehelp.in/Goa.jpeg",
+		price: "29,695",
+	},
+	{
+		id: 4,
+		name: "Varanasi",
+		info: "Varanasi is a holy city in northern India, situated on the banks of the sacred river Ganges. The city is a major pilgrimage site for Hindus and is famous for its ancient temples, winding alleyways, and colorful festivals.",
+		image: "https://cdn.thecodehelp.in/Varanasi.jpeg",
+		price: "31,095",
+	},
+	{
+		id: 5,
+		name: "Darjeeling",
+		info: "Starting in the colorful port city of Gdańsk, you'll escape the crowds and embrace the understated elegance of ready-for-prime-time Poland for 10 days. With an expert Rick Steves guide at your side, you'll experience mighty Malbork castle, the cobbly-cute village of Toruń, Poland's contemporary capital of Warsaw, the spiritual Jasna Góra Monastery, and charming Kraków — Poland's finest city. In this land of surprises — so trendy and hip, yet steeped in history — there's so much to discover. Join us for the Best of Poland in 10 Days!",
+		image: "https://cdn.thecodehelp.in/Darjeeling.jpeg",
+		price: "78,595",
+	},
+	{
+		id: 6,
+		name: "Jaisalmer",
+		info: "Jaisalmer is a city in the Indian state of Rajasthan, known for its stunning sandstone architecture and its location in the heart of the Thar Desert. The city is home to several impressive palaces and fortresses, as well as unique cultural traditions and handicrafts.",
+		image: "https://cdn.thecodehelp.in/Jaisalmer.jpeg",
+		price: "40,595",
+	},
+	{
+		id: 7,
+		name: "Kochi",
+		info: "Kochi is a port city in the Indian state of Kerala, known for its historic connections to trade and commerce. The city is home to several stunning churches, synagogues, and mosques, as well as a rich culinary tradition that blends Indian, Portuguese, and other influences.",
+		image: "https://cdn.thecodehelp.in/Kochi.jpeg",
+		price: "65,595",
+	},
+	{
+		id: 8,
+		name: "Chopta",
+		info: "Popularly referred to as the mini Switzerland of Uttarakhand, Chopta (at an approximate altitude of 2,608 m) is a slice of paradise tucked away in the Garhwal Himalayas. Surrounded by bugyals or velvety meadows and pristine snow-capped peaks, Chopta is an all-year holiday destination: pleasant in summer, rain-fresh in monsoon and a snow-clad fairyland in winter.",
+		image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRnavLFUNhZtzQsm5RVCzyFrim5akum_8whA&usqp=CAU",
+		price: "35,595",
+	},
+	{
+		id: 9,
+		name: "Mussoorie",
+		info: "Mussoorie, also known as Queen of the Hills, is among the most popular hill stations of the country. A Britisher, Captain Frederick Young, accompanied by an official named FJ Shore, had climbed up the hill from the Doon valley in 1827 and found this ridge offering great views and a salubrious climate.",
+		image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCZ_Oukjsm6EdhTpDXrJjg5OS5PvWt4nh7KA&usqp=CAU",
+		price: "45,595",
+	},
+	{
+		id: 10,
+		name: "Somnath",
+		info: "The Somnath temple, also called Somanātha temple or Deo Patan, is a Hindu temple located in Prabhas Patan, Veraval in Gujarat, India. It is one of the most sacred pilgrimage sites for Hindus and is the first among the twelve jyotirlinga shrines of Shiva.",
+		image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPvRJiOgTXY9-0EUjGxAm3BW64sO3P0Qyw1g&usqp=CAU",
+		price: "50,000",
+	},
+	
+	
+];
+
+export default data;
